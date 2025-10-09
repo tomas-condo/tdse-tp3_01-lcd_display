f36854364629880b38331ce56384f20d31d36d4c
tomas-condo <81434603+tomas-condo@users.noreply.github.com>
2025-10-09 18:41:30 -0300

    Update display.cpp

--- modules/display/display.cpp
+++ modules/display/display.cpp
@@ -58,16 +58,16 @@
 
 //=====[Declaration and initialization of public global objects]===============
 
-DigitalOut displayD0( D0 );
-DigitalOut displayD1( D1 );
-DigitalOut displayD2( D2 );
-DigitalOut displayD3( D3 );
-DigitalOut displayD4( D4 );
-DigitalOut displayD5( D5 );
-DigitalOut displayD6( D6 );
-DigitalOut displayD7( D7 );
-DigitalOut displayRs( D8 );
-DigitalOut displayEn( D9 );
+//DigitalOut displayD0( D0 );
+//DigitalOut displayD1( D1 );
+//DigitalOut displayD2( D2 );
+//DigitalOut displayD3( D3 );
+//DigitalOut displayD4( D4 );
+//DigitalOut displayD5( D5 );
+//DigitalOut displayD6( D6 );
+//DigitalOut displayD7( D7 );
+//DigitalOut displayRs( D8 );
+//DigitalOut displayEn( D9 );
 
 //=====[Declaration of external public global variables]=======================
 
@@ -223,7 +223,7 @@ static void displayPinWrite( uint8_t pinName, int value )
     switch( display.connection ) {
         case DISPLAY_CONNECTION_GPIO_8BITS:
             switch( pinName ) {
-                case DISPLAY_PIN_D0: displayD0 = value;   break;
+                /*case DISPLAY_PIN_D0: displayD0 = value;   break;
                 case DISPLAY_PIN_D1: displayD1 = value;   break;
                 case DISPLAY_PIN_D2: displayD2 = value;   break;
                 case DISPLAY_PIN_D3: displayD3 = value;   break;
@@ -234,19 +234,19 @@ static void displayPinWrite( uint8_t pinName, int value )
                 case DISPLAY_PIN_RS: displayRs = value;   break;
                 case DISPLAY_PIN_EN: displayEn = value;   break;
                 case DISPLAY_PIN_RW: break; 
-                default: break;
+                default: break;*/
             }
             break;
         case DISPLAY_CONNECTION_GPIO_4BITS:
             switch( pinName ) {
-                case DISPLAY_PIN_D4: displayD4 = value;   break;
+                /*case DISPLAY_PIN_D4: displayD4 = value;   break;
                 case DISPLAY_PIN_D5: displayD5 = value;   break;
                 case DISPLAY_PIN_D6: displayD6 = value;   break;
                 case DISPLAY_PIN_D7: displayD7 = value;   break;
                 case DISPLAY_PIN_RS: displayRs = value;   break;
                 case DISPLAY_PIN_EN: displayEn = value;   break;
                 case DISPLAY_PIN_RW: break; 
-                default: break;
+                default: break;*/
             }
             break;
     }
